e8b067204f7ed9b3b02ae44d5319d946db0ba662
rnazb <r.nazbat@gmail.com>
2021-09-07 21:51:39 +0800

    Update database config

--- config/database.js
+++ config/database.js
@@ -5,15 +5,13 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'strapi-mongo'),
         username: env('DATABASE_USERNAME', null),
         password: env('DATABASE_PASSWORD', null),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
