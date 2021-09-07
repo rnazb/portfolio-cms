1e292255fe372ea2cb31c844e8769992364d2f6e
rnazb <r.nazbat@gmail.com>
2021-09-07 22:24:49 +0800

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,24 @@
+const dbHost = process.env.NODE_ENV === 'production' ?
+  process.env.DATABASE_HOST : '127.0.0.1'
+const dbName = process.env.NODE_ENV === 'production' ?
+  process.env.DATABASE_NAME : 'strapi-mongo'
+const dbUsername = process.env.NODE_ENV === 'production' ?
+  process.env.DATABASE_USERNAME : null
+const dbPassword = process.env.NODE_ENV === 'production' ?
+  process.env.DATABASE_PASSWORD : null
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: dbHost,
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-mongo'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        database: dbName,
+        username: dbUsername,
+        password: dbPassword,
       },
       options: {
         ssl: env.bool('DATABASE_SSL', true),
