7d106dd1202e486cfc0e27f48724dcbcd4038012
rnazb <r.nazbat@gmail.com>
2021-09-08 15:11:02 +0800

    Add global find controller

--- api/global/controllers/global.js
+++ api/global/controllers/global.js
@@ -1,8 +1,19 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils')
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+ * Retrieve a record.
+ *
+ * @return {Object}
+ */
+
+  async find(ctx) {
+    const entity = await strapi.services.global.find();
+    return sanitizeEntity(entity, { model: strapi.models.global });
+  }
+}
