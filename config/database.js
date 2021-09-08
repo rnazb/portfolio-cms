af020f14c136b84125e2e9007b8cdaefe3dec8e9
rnazb <r.nazbat@gmail.com>
2021-09-08 10:40:01 +0800

    Reconfigure database.js

--- config/database.js
+++ config/database.js
@@ -1,25 +1,30 @@
-const dbHost = process.env.NODE_ENV === 'production' ?
-  process.env.DATABASE_HOST : '127.0.0.1'
-const dbName = process.env.NODE_ENV === 'production' ?
-  process.env.DATABASE_NAME : 'strapi-mongo'
-const dbUsername = process.env.NODE_ENV === 'production' ?
-  process.env.DATABASE_USERNAME : null
-const dbPassword = process.env.NODE_ENV === 'production' ?
-  process.env.DATABASE_PASSWORD : null
-const dbSrv = process.env.NODE_ENV === 'production' ? true : false
+// const dbHost = process.env.NODE_ENV === 'production' ?
+//   process.env.DATABASE_HOST : '127.0.0.1'
+// const dbName = process.env.NODE_ENV === 'production' ?
+//   process.env.DATABASE_NAME : 'strapi-mongo'
+// const dbUsername = process.env.NODE_ENV === 'production' ?
+//   process.env.DATABASE_USERNAME : null
+// const dbPassword = process.env.NODE_ENV === 'production' ?
+//   process.env.DATABASE_PASSWORD : null
+// const dbSrv = process.env.NODE_ENV === 'production' ? true : false
+const dbUri = process.env.NODE_ENV === 'production' ?
+  process.env.DATABASE_URI : 'mongodb://localhost:27017/strapi-mongo'
 
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
+      // settings: {
+      //   host: dbHost,
+      //   srv: dbSrv,
+      //   port: env.int('DATABASE_PORT', 27017),
+      //   database: dbName,
+      //   username: dbUsername,
+      //   password: dbPassword,
+      // },
       settings: {
-        host: dbHost,
-        srv: dbSrv,
-        port: env.int('DATABASE_PORT', 27017),
-        database: dbName,
-        username: dbUsername,
-        password: dbPassword,
+        uri: dbUri
       },
       options: {
         ssl: env.bool('DATABASE_SSL', false),
